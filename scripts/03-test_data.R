94c464e559878bf2bf63d0c9eb6b06700f08d64e
Cher Ning-Li <cher.ning@mail.utoronto.ca>
2024-09-19 18:05:20 -0400

    added sketches, cleaned data, added graphs into paper

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,21 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests data
+# Author: Cher Ning-Li
+# Date: 19 September 2024
+# Contact: cher.ning@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Must have data downloaded and cleaned
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test there are no negative number of marriages
+data$number_of_marriage |> min() <= 0
+
+# Test for NAs
+all(is.na(data$number_of_marriage))

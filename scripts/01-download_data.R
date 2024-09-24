79af24a7fe77247f5925a169cc0c8d9a3ce11089
Cher Ning-Li <cher.ning@mail.utoronto.ca>
2024-09-24 13:43:11 -0400

    download new data set

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,31 +1,29 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Toronto Open Data
 # Author: Cher Ning-Li
-# Date: 19 September 2024
+# Date: 24 September 2024
 # Contact: cher.ning@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
-library(tidyverse)
-
-#### Download data ####
+library(dplyr)
 
 # get package
-package <- show_package("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+package <- show_package("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
+package
 
 # get all resources for this package
-resources <- list_package_resources("e28bc818-43d5-43f7-b5d9-bdfb4eda5feb")
+resources <- list_package_resources("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
 
 # identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
 # load the first datastore resource as a sample
 data <- filter(datastore_resources, row_number()==1) %>% get_resource()
-
 
 #### Save data ####
 # change the_raw_data to whatever name you assigned when you downloaded it.
